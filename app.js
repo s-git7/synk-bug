4a80daebe0c46fd46dbff6968ce8c416e0293c79
Shrey <54481067+s-git7@users.noreply.github.com>
2022-02-03 23:53:40 +0530

    Delete app.js

--- app.js
+++
@@ -1,5 +0,0 @@
-function changeText() {
-    var name = document.getElementById("welcomeForm").fname.value;
-    var welcome = "Welcome, " + name;
-    document.getElementById("welcomeText").innerHTML = name;
-    }
